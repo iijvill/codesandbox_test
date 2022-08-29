140eb33dafd2b334ec2c5e142f188bc4e55b201a
いなむー <57438749+iijvill@users.noreply.github.com>
2022-08-29 17:22:43 +0900

    sec.20

--- src/index.js
+++ src/index.js
@@ -1,10 +1,45 @@
-import "./styles.css";
+// var val1 = "var1へんすう";
 
-document.getElementById("app").innerHTML = `
-<h1>Hello Vanilla!</h1>
-<div>
-  We use the same configuration as Parcel to bundle this sandbox, you can find more
-  info about Parcel 
-  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
-</div>
-`;
+// console.log(val1);
+
+// //var変数は上書き可能
+
+// val1 = "var変数を上書き";
+// console.log(val1);
+
+// // val1変数は再宣言可能
+
+// var val1 = "var変数を再宣言";
+
+// console.log(val1);
+
+// let val2 = "let変数";
+// console.log(val2);
+
+// //letも上書き可能
+
+// val2 = "let変数上書き";
+// console.log(val2);
+
+// let val2 = "変数宣言";
+
+// const val3 = "const変数";
+// console.log(val3);
+
+// val3 = "上書き！";
+
+// const val4 = {
+//   name: "aaa",
+//   age: 30,
+// };
+
+// // console.log(val4);
+
+// val4.name = "っっっｈ";
+// val4.address = "gggg";
+// console.log(val4);
+
+// const val5 = ["dog", "cat"];
+// val5[0] = "gergherhge";
+// val5.push("afefswef");
+// console.log(val5);
